6142fe399744a32e8307a57e19e9870a1b6e7755
PetrovaAnastasiya <33149053+PetrovaAnastasiya@users.noreply.github.com>
2020-07-05 13:43:40 +0300

    commit

--- CreatorGuidesSets/CreatorGuidesSets/CreatorGuidesSets.cpp
+++ CreatorGuidesSets/CreatorGuidesSets/CreatorGuidesSets.cpp
@@ -1,34 +1,8 @@
-﻿#include <fstream>
-#include <iostream>
-#include <string>
-#include <map>
-#include <regex>
-
-using namespace std;
-#define neterminal string
-
-bool openFile(ifstream& file, string fileName)
-{
-	file.open(fileName);
-	if (file.is_open())
-		return true;
-	else
-	{
-		return false;
-	}
-}
-
-string readInputData()
-{
-	const string FILE_IN_NAME = "input.txt";
-
-	ifstream fileForWork;
-	bool is_opened = openFile(fileForWork, FILE_IN_NAME);
-	
-	fileForWork.close();
-}
+﻿#include "Grammar.h"
 
 int main()
 {
-	string 
+	Grammar grammar("input.txt");
+	grammar.PrintGrammar("output.txt");
+	return 0;
 }
